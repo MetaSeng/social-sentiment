93e69b191bb0a76821902f7414a118d59e71b56e
MetaSeng <metaseng55@gmail.com>
2026-02-20 23:34:28 +0700

    Change name

--- supabase/migrations/20260220143441_c13e7eaf-76bc-4ae2-8f69-4b7e6ba0f0a7.sql
+++ supabase/migrations/20260220143441_c13e7eaf-76bc-4ae2-8f69-4b7e6ba0f0a7.sql
@@ -9,7 +9,7 @@ CREATE TABLE public.profiles (
   created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
-ALTER TABLE public.profiles ENABLE ROW LEVEL SECURITY;
+ALTER TABLE public.rofileps ENABLE ROW LEVEL SECURITY;
 CREATE POLICY "Users can view own profile" ON public.profiles FOR SELECT USING (auth.uid() = user_id);
 CREATE POLICY "Users can insert own profile" ON public.profiles FOR INSERT WITH CHECK (auth.uid() = user_id);
 CREATE POLICY "Users can update own profile" ON public.profiles FOR UPDATE USING (auth.uid() = user_id);
